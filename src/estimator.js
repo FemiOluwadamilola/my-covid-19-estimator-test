5df2ec8444edba88439be4b99b03e9055e9d4c21
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2020-04-10 11:58:13 +0100

    another coreection made

--- src/estimator.js
+++ src/estimator.js
@@ -5,7 +5,7 @@ const covid19ImpactEstimator = (data) => {
   const calCurrentInfected = (x) => currentInfected * x;
   const svcForImpact = (15 / 100) * (calCurrentInfected(10) * 1024);
   const svcForSevereImpact = (15 / 100) * (calCurrentInfected(50) * 1024);
-  return { 
+  return{ 
     data: input,
     impact: {
       currentInfected: calCurrentInfected(10),
@@ -14,7 +14,8 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: (35 / 100) * (input.totalHospitalBeds + svcForImpact),
       casesForICUByRequestedTime: (5 / 100) * (calCurrentInfected(10) * 1024),
       casesForVentilatorsByRequestedTime: (2 / 100) * (calCurrentInfected(10) * 1024),
-      dollarsInFlight: (calCurrentInfected(10) * 1024 * 0.65) * (1.5 * 30)},
+      dollarsInFlight: (calCurrentInfected(10) * 1024 * 0.65) * (1.5 * 30)
+    },
     severeImpact: {
       currentInfected: calCurrentInfected(50),
       infectionsByRequestedTime: calCurrentInfected(50) * 1024,
@@ -22,6 +23,9 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: (35 / 100) * (input.totalHospitalBeds + svcForSevereImpact),
       casesForICUByRequestedTime: (5 / 100) * (calCurrentInfected(50) * 1024),
       casesForVentilatorsByRequestedTime: (2 / 100) * (calCurrentInfected(50) * 1024),
-      dollarsInFlight: (calCurrentInfected(50) * 1024 * 0.65) * (1.5 * 30)}}
-  };
+      dollarsInFlight: (calCurrentInfected(50) * 1024 * 0.65) * (1.5 * 30)
+}
+};
+};
+
   export default covid19ImpactEstimator;
