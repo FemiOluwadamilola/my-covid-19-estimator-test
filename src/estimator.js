6ec9efe999c852467b4087003789db8ebcc53cea
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2020-04-10 10:46:04 +0100

    covid-19 estimator challenge 1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,5 @@
 const covid19ImpactEstimator = (data) => {
-  const input = {
-    region:{
-      name:"africa",
-      avgAge:19.7,
-      avgDailyIncomeInUSD:5,
-      avgDailyIncomePopulation:0.71
-    },
-    periodType:'days',
-    timeToElapse:58,
-    reportedCases:674,
-    population:66622705,
-    totalHospitalBeds:1380614
-  }
+  const input = data;
   const infected = input.reportedCases;
   var currentInfected = infected;
   // function to get currentInfected 
